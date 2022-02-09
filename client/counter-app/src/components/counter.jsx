ff6ce0ed5fa99f3df0d38f587ce1f0069572e549
WillTrem <wt22@videotron.ca>
2022-02-08 20:54:51 -0500

    Experimenting with react

---
+++ client/counter-app/src/components/counter.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class counter extends Component {
+	state = { count: 0 };
+	render() {
+		return (
+			<React.Fragment>
+				<span className="badge bg-primary m-2">{this.formatCount()}</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
+			</React.Fragment>
+		);
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "ZERO" : count;
+	}
+}
+
+export default counter;
