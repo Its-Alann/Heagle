2c33b3236695412f373201fc6488ad4ed98ea3b5
WillTrem <wt22@videotron.ca>
2022-02-16 14:02:23 -0500

    Create db.js

---
+++ server/db.js
@@ -0,0 +1,8 @@
+const mySql = required('mysql')
+
+const db = mySql.createPool({
+	host: "l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "w3e8oegzwefulssk",
+	password: "eno5b8w3kezdfqn2",
+	database: "e5zkwad79wtbvjrc"
+})
