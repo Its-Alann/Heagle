87dcbfac8bfb8df0c17ea53c075f49ace244304d
WillTrem <wt22@videotron.ca>
2022-02-17 11:18:50 -0500

    Update db.js
    Added export of the module

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-const mySql = required('mysql')
+const mySql = require('mysql')
 
 const db = mySql.createPool({
 	host: "l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
@@ -6,3 +6,5 @@ const db = mySql.createPool({
 	password: "eno5b8w3kezdfqn2",
 	database: "e5zkwad79wtbvjrc"
 })
+
+module.exports = {db}; //Exports the db.js file as a module so that other .js files in the server can use const db
