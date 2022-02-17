c9d6f5e1b28dbadde95a71a605c0d80cd405269e
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2022-02-17 13:34:10 -0500

    Added a temp database for products

---
+++ ecommerce-website/src/tempItemsDatabase.js
@@ -0,0 +1,45 @@
+
+const items = [
+
+    {
+        id: 1,
+        name: "Name 1",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        price: 0,
+        currency: "$",
+        image: ""
+
+    },
+
+    {
+        id: 2,
+        name: "Name 2",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        price: 0,
+        currency: "$",
+        image: ""
+
+    },
+
+    {
+        id: 3,
+        name: "Name 3",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        price: 0,
+        currency: "$",
+        image: ""
+
+    },
+
+    {
+        id: 4,
+        name: "Name 4",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        price: 0,
+        currency: "$",
+        image: ""
+
+    }
+
+
+]
