19a80a965d6b053b9e4a3d583d6c3c930f18e99e
WillTrem <wt22@videotron.ca>
2022-02-18 19:33:51 -0500

    tested out some stuff for backend

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Axios from 'axios';
+import { useState } from "react";
+
 
 function App() {
+  //States
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const registerUser = ()=>{
+    Axios.post("https://localhost:3001/registerUser", {
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName
+    }).then(()=>{
+      console.log("User successfully registered")
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Heagle.com is under construction
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Email</label>
+      <input type="text" onChange={(event)=> setEmail(event.target.value)}></input>
+      <label >Password</label>
+      <input type="text" onChange={(event)=> setPassword(event.target.value)}></input>
+      <label>FirstName</label>
+      <input type="text" onChange={(event)=> setFirstName(event.target.value)}></input>
+      <label>LastName</label>
+      <input type="text" onChange={(event)=> setLastName(event.target.value)}></input>
+      <button onClick={registerUser}>Register</button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,44 @@
 const express = require("express"); //Imports express
 const app = express();
+const cors = require("cors");
+const db = require("./db");
+const database = db.db;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.use(express.json())
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+  
+//api endpoints 
+
+app.get("/", (req, res) => { //req is the request (input) and res is the response (output)
+	res.send("This is the heagle backend. You can access the main website at this URL: https://heagle.herokuapp.com");
 });
+
+app.post("/registerUser", (req, res) =>{
+	const email = req.body.email;
+	const password = req.body.password;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+
+	const sqlQuery = 
+		`INSERT INTO e5zkwad79wtbvjrc.temp_users (email, password, firstName, lastName)
+		VALUES (?,?,?,?)`;
+
+	database.query(sqlQuery, [email, password, firstName, lastName], (err, result)=>{
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.send("User Successfully Registered")
+			console.log(result);
+		}
+	})
+})
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
