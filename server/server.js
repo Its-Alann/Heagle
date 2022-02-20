7dd3b0f5342453aca664f5b2e911553e354a18e7
WillTrem <wt22@videotron.ca>
2022-02-20 12:44:51 -0500

    Fixed backend issue
    Fixed backend issue and added some more api endpoints that I was working on on my own

--- server/server.js
+++ server/server.js
@@ -7,10 +7,19 @@ const db = require('./db');
 app.use(cors());
 app.use(express.json()); 
 
-//connect to mysql
-app.listen(3001, () => {
-	// console.log("Server in running on part 3001")
-})
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+  
+//-----------------------------api endpoints----------------------------------------------------//
+
+//Root endpoint (when accessing the base url of the server)
+app.get("/", (req, res) => { //req is the request (input) and res is the response (output)
+	res.send("This is the heagle backend. You can access the main website at this URL: https://heagle.herokuapp.com");
+});
 
 app.get("/getProduct/:id", (req, res) => {
 	const searchId = req.params.id;
@@ -24,5 +33,42 @@ app.get("/getProduct/:id", (req, res) => {
   	}) 
 })
 
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT);
+
+app.get("/fetchProductList", (req, res)=>{
+	const sqlQuery = 
+	`SELECT * FROM  e5zkwad79wtbvjrc.products
+	`
+	database.query(sqlQuery, (err, result) =>{
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.send(result);
+		}
+	})
+})
+
+app.post("/registerUser", (req, res) =>{
+	
+	const email = req.body.email;
+	const password = req.body.password;
+	const firstName = req.body.firstName;
+	const lastName = req.body.lastName;
+
+	const sqlQuery = 
+		`INSERT INTO e5zkwad79wtbvjrc.temp_users (email, password, firstName, lastName)
+		VALUES (?,?,?,?)`;
+
+	database.query(sqlQuery, [email, password, firstName, lastName], (err, result)=>{
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.send("User Successfully Registered")
+			console.log(result);
+		}
+	})
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
