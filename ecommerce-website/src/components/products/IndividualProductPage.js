c8c8cc0c99c7bc3886c7f9b6c389de0833905865
georgia1515 <georgiapitic@gmail.com>
2022-02-20 20:17:26 -0500

    Added const for local coding

--- ecommerce-website/src/components/products/IndividualProductPage.js
+++ ecommerce-website/src/components/products/IndividualProductPage.js
@@ -19,7 +19,9 @@ const IndividualProductPage = ({ match }) => {
   useEffect(()=> {
       const getProductFromServer = "https://heagle-backend.herokuapp.com/getProduct/" + pageId.id
       Axios.get(getProductFromServer).then((response) => {
-          setName(response.data[0].name);
+      // Use this const below for when you code in local, comment it out once done
+      //const getProductFromServer = "http://localhost:3001/getProduct/" + pageId.id  
+        setName(response.data[0].name);
           setDescription(response.data[0].description);
           setImageUrl(response.data[0].imageUrl);
           setPrice(response.data[0].price);
