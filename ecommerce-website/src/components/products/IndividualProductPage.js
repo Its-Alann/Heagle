34e564ebb97579a722dd2f76f80ebee1aa78868e
Aykchen <aliceykchen01@gmail.com>
2022-03-02 20:32:51 -0500

    Improved the design of displaying single product
    - focused on the image's size and the +, - buttons design
    - refined the display of the product's info

--- ecommerce-website/src/components/products/IndividualProductPage.js
+++ ecommerce-website/src/components/products/IndividualProductPage.js
@@ -2,7 +2,7 @@ import React from "react";
 import './Products.css'
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import {Button, Container, Row} from 'react-bootstrap'
+import {Button, Container, Row, Col} from 'react-bootstrap'
 
 
 import Axios from "axios";
@@ -29,86 +29,91 @@ const IndividualProductPage = ({ match }) => {
   }, []);
 
   return (
-    <>
-      <Container>
-      <div className="single-product">
+      <Container className="single-product">
         <Row>
-          <div className="col-md-6">
-            <div className="single-image">
-              <img src={selectedProductImageUrl} alt={selectedProductName} />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="product-dtl">
-              <div className="product-info bg-light">
-                <div className="product-name bg-light">{selectedProductName}</div>
+          <Col className="align-self-center">
+              {/* Product Image */}
+              <div className="row-md-6 single-image">
+                <img src={selectedProductImageUrl} className = "img-fluid" alt={selectedProductName} />
               </div>
-              <div>
-                <div className="mb-5 mb-md-3 bg-light p-3 shadow-sm rounded">
-                  {/* <h6>Price</h6> */}
-                  <span>${selectedProductPrice}</span>
-                  <div className="alert  mt-3">
+
+              {/* Product Description */}
+              <div className="row-md-6 SingleProductDescription">
+                <p>{selectedProductDescription}</p>
+              </div>
+          </Col>
+
+          <Col>
+            <div className="row-md-6 product-dtl">
+              {/* Product Name */}
+              <div className="product-name bg-light">{selectedProductName}</div>
+              
+              <div className="mb-5 mb-md-3 bg-light p-3 shadow-sm rounded">
+                {/* Product Price */}
+                <div className="product-count">${selectedProductPrice}</div>
+
+                  {/* Product Status */}
+                  {/* <div className="alert mt-3">
                     <h6>Status</h6>
                     <span>In Stock</span>
-                  </div>
+                  </div> */}
 
-                  <div className="minus-button">
-                    <button className="MinusButton">-</button>
+                {/* Product Quantity */}
+                <Row className="justify-content-md-center">
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <button className="qty-button">+</button>
                   </div>
-                  <div className="quantity">
-                    <p className="quantityNum" id="quantity">1</p>
+                    
+                  <div className="col-md-4 quantity ">
+                    <p className="alert">1</p>
                   </div>
-                  <div className="add-button">
-                    <button className="AddButton">+</button>
+                    
+                  <div className="col-md-4 d-flex justify-content-center">
+                    <button className="qty-button">-</button>
                   </div>
-                  
+                </Row>
 
-                  <div className="alert  mt-3">
-                    <button className="SingleProductButton">Add To Cart</button>
-                  </div>
+                <div className="alert mt-3">
+                  <button className="SingleProductAddCartButton">Add To Cart</button>
                 </div>
               </div>
             </div>
-          </div>
-        </Row>
 
-          {/* RATING */}
-        <Row>
-          <div className="col-md-6 SingleProductDescription">
-            <p>{selectedProductDescription}</p>
-          </div>
-          <div className="col-md-6">
-            <h6 className="mb-3">REVIEW(S)</h6>
-              <div className="mb-5 mb-md-3 bg-light p-3 shadow-sm rounded">
-                <strong>John Doe</strong>
-                <span> Feb 17 2022</span>
-                <div className="alert alert-info mt-3">
-                  Yes, I would recommend this to a friend.
+            {/* Product Review(s) */}
+            <div className="row-md-6">
+              <h6 className="mb-3">REVIEW(S)</h6>
+                <div className="mb-5 mb-md-3 bg-light p-3 shadow-sm rounded">
+                  <strong>John Doe</strong>
+                  <span> Feb 17 2022</span>
+                  <div className="alert alert-info mt-3">
+                    Yes, I would recommend this to a friend.
+                  </div>
                 </div>
-              </div>
-          </div>
-          {/* <div className="col-md-6">
-            <h6>WRITE A CUSTOMER REVIEW</h6>
-            <div className="my-4"></div>
-            <form>
-              <div className="my-4">
-                <strong>Comment</strong>
-                <textarea
-                  row="3"
-                  className="col-12 bg-light p-3 mt-2 border-0 rounded"
-                ></textarea>
-              </div>
-              <div className="my-3">
-                <button className="col-12 bg-black border-0 p-3 rounded text-white">
-                  SUBMIT
-                </button>
-              </div>
-            </form>
-          </div> */}
+            </div>
+
+            {/* Product - Add a Review (Additional Feature) */}
+            {/* <div className="col-md-6">
+              <h6>WRITE A CUSTOMER REVIEW</h6>
+              <div className="my-4"></div>
+              <form>
+                <div className="my-4">
+                  <strong>Comment</strong>
+                  <textarea
+                    row="3"
+                    className="col-12 bg-light p-3 mt-2 border-0 rounded"
+                  ></textarea>
+                </div>
+                <div className="my-3">
+                  <button className="col-12 bg-black border-0 p-3 rounded text-white">
+                    SUBMIT
+                  </button>
+                </div>
+              </form>
+            </div> */}
+
+          </Col>
         </Row>
-      </div>
       </Container>
-    </>
   );
 };
 export default IndividualProductPage;
