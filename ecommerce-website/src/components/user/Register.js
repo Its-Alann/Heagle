9f1c21bdee286777d11456e752498b779d1ee2e1
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2022-03-20 19:18:19 -0400

    Added register function

--- ecommerce-website/src/components/user/Register.js
+++ ecommerce-website/src/components/user/Register.js
@@ -2,9 +2,84 @@ import React from 'react'
 import './Login.css'
 import './Register.css'
 import {Card, Button, Container, Row} from 'react-bootstrap'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import baseUrl from "../../SystemVariables";
+
 
 const Register = () => {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [email, setEmail] = useState("");
+    const [emailConfirm, setEmailConfirm] = useState({});
+    const [phoneNumber, setPhone] = useState("");
+    const [errMessage, setErrMessage] = useState("");
+    const [formConfirm, setFormConfirm] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        if (firstName == "") {
+            setErrMessage("Please enter your first name")
+            setFormConfirm(false)
+        }
+
+        else if (password == "") {
+            setErrMessage("Please enter a password")
+            setFormConfirm(false)
+        }
+
+        else if (passwordConfirm == "") {
+            setErrMessage("Please confirm the password")
+        }
+
+        else if (email == "") {
+            setErrMessage("Please enter an email address")
+            setFormConfirm(false)
+        }
+
+        else if (phoneNumber == "") {
+            setErrMessage("Please enter a phone number")
+        }
+        
+        else {
+            setErrMessage("")
+            setFormConfirm(true)
+        }
+        
+    }, [firstName, password, passwordConfirm, email, phoneNumber])
+    
+    const handleSignUp = () => {
+        
+        const url = baseUrl + "/registerUser"
+
+        
+        if (password != passwordConfirm) {
+            setErrMessage("Passwords do not match")
+        }
+        
+        else if (password === passwordConfirm && formConfirm == true) {
+            setErrMessage("")
+
+            Axios.post(url, {
+    
+                firstName: firstName,
+                lastName: lastName,
+                password: password,
+                email: email,
+                phoneNumber: phoneNumber
+            }).then((console.log("Success")))
+            navigate("/login");
+    
+        }
+        
+        // console.log(user)
+    }
+
     return(
         <div className="page">
             <h1>Sign Up</h1>
@@ -20,17 +95,30 @@ const Register = () => {
                 </div>
                 <div className="login-block-center">
                     <form>
-                        <input type="text" placeholder='Username' />
-                        <input type="password" placeholder='Password'/>
-                        <input type="password" placeholder='Reenter Password'/>
-                        <input type="text" placeholder='Email'/>
-                        <input type="text" placeholder='Address'/>
-                        <input type="text" placeholder='Phone number'/>
+                        <input type="text" placeholder='First Name' onChange={({ target }) => {
+                            setFirstName(target.value);
+                        }}/>
+                        <input type="text" placeholder='Last Name' onChange={({ target }) => {
+                            setLastName(target.value);
+                        }}/>
+                        <input type="password" placeholder='Password' onChange={({ target }) => {
+                            setPassword(target.value);
+                        }}/>
+                        <input type="password" placeholder='Reenter Password' onChange={({ target }) => {
+                            setPasswordConfirm(target.value);
+                        }}/>
+                        <input type="text" placeholder='Email' onChange={({ target }) => {
+                            setEmail(target.value);
+                        }}/>
+                        <input type="text" placeholder='Phone number' onChange={({ target }) => {
+                            setPhone(target.value);
+                        }}/>
                     </form>
+                    <h6 className="error-message">{errMessage}</h6>
                 </div>
                 <div className="login-block-bottom">
                     <div className="button">
-                        <Button className="btn"> Sign Up </Button>
+                        <Button className="btn" onClick={handleSignUp}> Sign Up </Button>
                     </div>
                     <div className="create-account">
                         <Link exact to="/login">Already have an account? Log In now.</Link>
