b6f3145aafbb19da33a7e51c478a499750c14185
Aykchen <aliceykchen01@gmail.com>
2022-03-20 23:57:14 -0400

    Changed url

--- ecommerce-website/src/SystemVariables.js
+++ ecommerce-website/src/SystemVariables.js
@@ -1,3 +1,3 @@
-// const baseUrl = "https://heagle-backend.herokuapp.com";
-const baseUrl = "http://localhost:3001"
+const baseUrl = "https://heagle-backend.herokuapp.com";
+// const baseUrl = "http://localhost:3001"
 export default baseUrl;
