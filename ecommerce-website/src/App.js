fdaae04de0c43a16759b0a7b52ae8f3a3df3b1c1
harri012 <harrisson.tang12@gmail.com>
2022-03-20 21:38:31 -0400

    Update App.js

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -19,7 +19,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/home/Home";
 import ProductLoader from "./components/products/productLoader";
 import SellerProducts from "./components/user/SellerProducts";
-import User from "./components/user/User"
+import User from "./components/user/User";
 
 function App() {
 	//States
@@ -52,7 +52,7 @@ function App() {
 					<Route path="/clothes/:id" element={<IndividualProductPage />} />
 					<Route path="/food/:id" element={<IndividualProductPage />} />
 					<Route path="/login" exact element={<Login/>} />
-					<Route path="/user" element={<User/>}/>
+					<Route path="/user" element={<User/>} />
 				</Routes>
 				{/* <h2> Food </h2> */}
 
