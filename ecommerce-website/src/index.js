6c099d7d02191a1fae98901370a2907cfec43c95
Emily <em_dang@encs.concordia.ca>
2022-04-04 21:02:20 -0400

    Create Payment page

--- ecommerce-website/src/index.js
+++ ecommerce-website/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap'
 import 'react-router-dom'
+import 'bootstrap/dist/js/bootstrap.js';
 
 
 ReactDOM.render(
