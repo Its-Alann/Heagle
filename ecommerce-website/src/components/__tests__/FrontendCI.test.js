58f1136e7be68001e3f96b3dba76102410066378
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2022-04-15 15:14:48 -0400

    Added new and improved tests cases

--- ecommerce-website/src/components/__tests__/FrontendCI.test.js
+++ ecommerce-website/src/components/__tests__/FrontendCI.test.js
@@ -1,12 +1,19 @@
 
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUsedNavigate,
+}));
+
 import { render, screen, getByText, getByRole, getByLabelText } from "@testing-library/react"
 import { renderer } from "react-test-renderer"
 import IndividualProductPage from "../products/IndividualProductPage"
 import Login from "../user/Login"
 import App from "../../App"
 import SellerProducts from "../user/SellerProducts"
-import { Router } from "react-router-dom"
+import { MemoryRouter, Router } from "react-router-dom"
 import Register from "../user/Register"
+import ShoppingCart from "../cart/ShoppingCart"
 
 describe('Sample Tests', () => {
 
@@ -21,42 +28,47 @@ describe('Sample Tests', () => {
 
 describe('Browsing Items Tests', () => {
 
+    test('Loads product cards on individual page', () => {
     
-    test('Loads product cards on home screen', () => {
-    
-        render(<IndividualProductPage />)
+        const { getById } = render(<IndividualProductPage />, { wrapper: MemoryRouter })
         screen.getByText(/Yes, I would recommend this to a friend./i)
-    
+        screen.getAllByText("+")
+        screen.getAllByText("-")
+        screen.getAllByText(/Add to Cart/i)
         });
 })
 
 
 describe('Managing Profiles Tests', () => {
 
-    // test('Renders the login buttton on home page', () => {
-    //     render(<App />);
-    //     const linkElement = screen.getByText(/login/i);
-    //     expect(linkElement).toBeInTheDocument();
-    // });
-    
-    // test('Renders the register buttton on home page', () => {
-    //     render(<App />);
-    //     const linkElement = screen.getByText(/register/i);
-    //     expect(linkElement).toBeInTheDocument();
-    // });
-
-    test("Renders register page", () => {
-        <Router>
-            render(<Register/>)
-        </Router>
+    test("Renders Login page", () => {
+        const { getById } = render(<Login />, { wrapper: MemoryRouter })
+        screen.getAllByText(/Login/i)
+        screen.getAllByText(/Connect to your account/i)
     })
 
     test('Login form appears', () => {
         
-        <Router>
-            render(<Login/>)
-        </Router>
+        const { getById } = render(<Login />, { wrapper: MemoryRouter })
+        screen.getByPlaceholderText("Email address")
+        screen.getByPlaceholderText("Password")
     
+    })
+    
+    
+    test("Renders Register page", () => {
+        const { getById } = render(<Register />, { wrapper: MemoryRouter })
+        screen.getAllByText(/Sign Up/i)
+    })
+    
+    test("Renders boxes for information input", () => {
+        const { getById } = render(<Register />, { wrapper: MemoryRouter })
+        screen.getByPlaceholderText("First Name")
+        screen.getByPlaceholderText("Last Name")
+        screen.getByPlaceholderText("Password")
+        screen.getByPlaceholderText("Reenter Password")
+        screen.getByPlaceholderText("Email")
+        screen.getByPlaceholderText("Phone Number")
     })
 })
 
@@ -64,10 +76,28 @@ describe('Sellers Tests', () => {
 
     test('Shows the products for sellers', () => {
 
-    <Router>
-        render(<SellerProducts/>)
-    </Router>
+        const { getById } = render(<SellerProducts />, { wrapper: MemoryRouter })
     })
 })
 
+describe('Shopping Cart Tests', () => {
+
+    test('Renders shopping cart page', () => {
+
+        const { getById } = render(<ShoppingCart />, { wrapper: MemoryRouter })
+        screen.getAllByText(/Shopping Cart/i)
+    })
+
+    test('Shows items in the shopping cart page', () => {
+
+        const { getById } = render(<ShoppingCart />, { wrapper: MemoryRouter })
+        screen.getByText(/My Items/i)
+    })
+
+    test('Shows summary of prices', () => {
+
+        const {getById} = render(<ShoppingCart/>, {wrapper: MemoryRouter})
+        screen.getByText(/Summary/i)
+    })
+})
 
